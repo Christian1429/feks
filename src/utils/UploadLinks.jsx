aa135e50e9ec28808d1efa964e60efc92d8916cf
Christian <christianlindberg01@hotmail.com>
2025-07-13 14:11:22 +0200

    typo fix

--- src/utils/UploadLinks.jsx
+++ src/utils/UploadLinks.jsx
@@ -1,4 +1,4 @@
-import { uploadImage } from '../../api/article';
+import { uploadImage } from '../api/article';
 
 export async function handleImageUpload(e, setPreview, setLoading) {
   const selectedFile = e.target.files[0];
